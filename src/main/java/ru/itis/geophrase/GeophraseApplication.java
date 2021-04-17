468e8b7c3e04d5b48fea9f58f37a10215e33b2c8
saydar31 <saidar31@yandex.ru>
2021-04-17 18:32:05 +0300

    init project

---
+++ src/main/java/ru/itis/geophrase/GeophraseApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.geophrase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeophraseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeophraseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/geophrase/GeophraseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.geophrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeophraseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
